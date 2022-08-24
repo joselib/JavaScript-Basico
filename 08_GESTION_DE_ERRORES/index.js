190c08f2f53f71406dcd880b9cf33c1ba1ff2bfa
joselib <54868005+joselib@users.noreply.github.com>
2022-08-24 00:41:05 -0500

    proyecto con node y winston

---
+++ 08_GESTION_DE_ERRORES/index.js
@@ -0,0 +1,11 @@
+const logger = require('./logger');
+
+
+
+
+
+// console.log('Hello World');
+logger.info('esto es una info');
+logger.warn('esto es una advertencia');
+logger.error('esto es un error');
+logger.debug('esto es un debug');
